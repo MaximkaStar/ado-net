d37c4897b915c0ce7aa74fce08c81bb5f06e7843
MaximkaStar <max.martsinkus@mail.ru>
2019-01-13 10:38:29 +0600

    initial commit

---
+++ EntitiyFramework/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiyFramework
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() : base("DbConnection") {}
+        
+        public DbSet<Person>  persons { get; set; } 
+    }
+}
